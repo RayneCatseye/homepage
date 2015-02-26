0a1593b171e000db7604a90907130e9f0b63af3f
Emily Saunders Walmsley <EmSaundWalm@gmail.com>
2015-02-26 22:01:56 +1100

    Removed extranious/legacy code no longer in use

--- app/views.py
+++ app/views.py
@@ -29,12 +29,6 @@ def about():
 def publickey():
     return render_template('css/bootstrap.css')
 
-# Oh dear Im a lazy person
-@app.route('/zalgo/')
-def zalgo():
-    return render_template('zalgorithm.html',
-        title= 'Z Algorithm')
-
 @app.route('/keybase.txt')
 def keybase():
     return render_template('keybase.txt')
